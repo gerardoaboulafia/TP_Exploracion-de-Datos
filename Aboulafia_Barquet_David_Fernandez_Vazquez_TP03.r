792321b2159a28c8fa128ab10dc7c8dcbd3e668d
Gerardo Aboulafia <gerardoaboulafia@uca.edu.ar>
2023-10-23 00:11:55 -0300

    trabajo final tentativo

--- Aboulafia_Barquet_David_Fernandez_Vazquez_TP03.r
+++ Aboulafia_Barquet_David_Fernandez_Vazquez_TP03.r
@@ -21,7 +21,6 @@ libs <- c(
   "ROCR",
   "caret",
   "bestglm",
-  "InformationValue",
   "pROC",
   "glmnet"
 )
@@ -66,15 +65,10 @@ sum(is.na(WineQT))
 WineQT <- subset(WineQT, select = -Id)
 
 #Normalización de los datos
-WineQT <- WineQT %>%
+WineQT_scaled <- WineQT %>%
   mutate_if(is.numeric, scale)
 
 # -------------Aplicación de modelo geométrico de clasificación (K-means)------------------#
-# Instanciamos el modelo con 3 clusters
-set.seed(123)
-km_inicial <- kmeans(WineQT, centers = 3, nstart = 20)
-km_inicial
-
 # Iteramos sobre el modelo para obtener el mejor valor de clusters
 n_clusters <- 10
 
@@ -84,7 +78,7 @@ wss <- numeric(n_clusters)
 # Iteramos sobre el modelo para obtener el mejor valor de clusters
 for (i in 1:n_clusters) {
   # Ajustamos el modelo: km_model
-  km_model <- kmeans(WineQT, centers = i, nstart = 50)
+  km_model <- kmeans(WineQT_scaled, centers = i, nstart = 20)
   # Guardamos el resultado de WSS
   wss[i] <- km_model$tot.withinss
 }
@@ -93,10 +87,10 @@ for (i in 1:n_clusters) {
 wss_df <- tibble(clusters = 1:n_clusters, wss = wss)
 
 scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
-  geom_point(size = 4)+
+  geom_point(size = 4) +
   geom_line() +
   scale_x_continuous(breaks = c(2, 4, 6, 8, 10)) +
-  xlab("Number of clusters")
+  xlab("Número de clusters")
 
 scree_plot
 
@@ -106,46 +100,17 @@ scree_plot +
     linetype = "dashed",
 )
 
-
-# -------------Aplicación de modelo probabilístico de clasificación (Bayes)------------------#
+# -------------Aplicación de modelo probabilístico de clasificación (Regresión Logística)------------------#
 # Fijamos los valores categóricos de la variable 'quality'
 c <- ifelse(WineQT$quality >= 6, 1, 0)
 WineQT$quality <- c
 # Modelo de ML probabilístico usando logit:
 # ('quality' es una variable binaria)
-df$quality <- as.factor(df$quality)
-
-# Separamos los datos en datos de entrenamiento y de prueba
-#(70% entrenamiento, 30% prueba)
+WineQT$quality <- as.factor(WineQT$quality)
 
 # Seteamos una seed para poder reproducir los resultados
 set.seed(123)
 
-split <- sample.split(df$quality, SplitRatio = 0.7)
-train_data <- subset(df, split == TRUE)
-test_data <- subset(df, split == FALSE)
-
-# Instanciamos el modelo
-logreg_model <- glm(
-  quality ~ .,
-  data = train_data,
-  family = binomial(link = "logit")
-)
-
-# Generamos las predicciones sobre los datos de testeo
-predictions <- predict(logreg_model, newdata = test_data, type = "response")
-
-# Evaluación del modelo
-# Empleamos ROC y AUC
-pred <- prediction(predictions, test_data$quality)
-perf <- performance(pred, "tpr", "fpr")
-auc <- as.numeric(performance(pred, "auc")@y.values)
-
-# AUC value
-cat("AUC:", auc, "\n")
-
-
-# -------------Aplicación de modelo de lógico de clasificación (Decision Tree Classifier)------------------#
 samp <- sample(1:nrow(WineQT), 0.7 * nrow(WineQT))
 train <- WineQT[samp, ]
 test <- WineQT[-samp, ]
@@ -157,6 +122,7 @@ model <- glm(
   family = binomial(link = "logit")
 )
 
+# Instanciamos el mejor modelo
 best_model <- bestglm(
   train,
   IC = "AIC",
@@ -165,3 +131,82 @@ best_model <- bestglm(
 )
 
 best_model$Subsets
+
+best_train <- glm(quality ~ fixed.acidity + volatile.acidity + citric.acid + total.sulfur.dioxide + sulphates + alcohol, 
+  data = train, 
+  family = binomial)
+
+best_pred <- predict(best_train, test, type = "response")
+
+# Convertir las probabilidades en etiquetas binarias (0 o 1) usando un umbral (por ejemplo, 0.5)
+test_pred_binary <- ifelse(best_pred >= 0.5, 1, 0)
+
+# Crear una matriz de confusión
+confusion_matrix <- confusionMatrix(data = factor(test_pred_binary), reference = factor(test$quality))
+
+# Mostrar la matriz de confusión
+print(confusion_matrix)
+
+ROC_best_glm <- roc(test$quality, best_pred)
+auc(ROC_best_glm)
+plot(ROC_best_glm, col = "red")
+
+# -------------Aplicación de modelo de lógico de clasificación (Decision Tree Classifier)------------------#
+library(rpart)
+library(tidymodels)
+
+# Separamos los datos en datos de entrenamiento y de prueba
+#(70% entrenamiento, 30% prueba)
+samp <- sample(1:nrow(WineQT), 0.7 * nrow(WineQT))
+train <- WineQT[samp, ]
+test <- WineQT[-samp, ]
+
+data_split <- initial_split(WineQT, prop = 0.7)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+# Instanciamos el modelo
+tree_class <- rpart(
+  quality ~ .,
+  data = train_data,
+  method = "class"
+)
+
+tree_class
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+# Graficamos el árbol
+fancyRpartPlot(tree_class)
+
+# Evaluamos el modelo
+important_variables <- tree_class$variable.importance
+important_variables <- as.data.frame(important_variables)
+important_variables
+printcp(tree_class)
+
+# Usamos el modelo para predecir
+test_data$quality_pred <- predict(tree_class, test_data, type = "class")
+test_data$quality_pred_prob <- predict(tree_class, test_data, type = "prob")
+test_data
+
+# Generamos una matriz de confusión
+confusionMatrix(
+  test_data$quality,
+  test_data$quality_pred,
+  positive = "1"
+)
+
+# Hacer curva ROC
+library(ROCR)
+ROC_best_DT <- roc(test_data$quality, test_data$quality_pred_prob[, 2])
+
+# Calcular AUC
+auc(ROC_best_DT)
+
+
+### -------------Selección del mejor modelo de clasificación------------------#
+# Plot de las curvas ROC de los modelos
+plot(ROC_best_glm, col = "red") # Regresión Logística
+plot(ROC_best_DT, col = "blue", add = TRUE) # Decision Tree
